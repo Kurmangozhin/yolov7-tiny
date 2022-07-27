f6530892ae27278489a93978495cddc053902ef0
kurmangozhin <kurmangozhintimur1989@gmail.com>
2022-07-27 11:20:05 +0600

    27072022

---
+++ main.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <queue>
+#include <iterator>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/all_layers.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/cuda.hpp>
+
+
+
+
+using namespace cv;
+using namespace dnn;
+using namespace std;
+using namespace cuda;
+
+
+
+const float CONFIDENCE_THRESHOLD = 0.5;
+const float NMS_THRESHOLD = 0.4;
+const int NUM_CLASSES = 1;
+
+
+
+const cv::Scalar colors[] = {
+    {0, 255, 255},
+    {255, 255, 0},
+    {0, 255, 0},
+    {255, 0, 0}
+};
+
+const auto NUM_COLORS = sizeof(colors) / sizeof(colors[0]);
+const std::string weights = "yolov7.weights";
+const std::string cfg = "yolov7.cfg";
+const std::string class_path = "classes.txt";
+
+
+
+int main()
+{
+
+    std::vector<std::string> class_names;
+
+
+
+    std::ifstream class_file(class_path);
+    if (!class_file)
+    {
+        std::cerr << "failed to open classes.txt\n";
+        return 0;
+    }
+
+    std::string line;
+    while (std::getline(class_file, line))
+        class_names.push_back(line);
+
+
+    auto net = cv::dnn::readNet(cfg, weights);
+    auto output_names = net.getUnconnectedOutLayersNames();
+
+    cv::Mat frame, blob;
+    std::vector<cv::Mat> detections;
+
+    frame = cv::imread("1.jpg");
+
+    cv::dnn::blobFromImage(frame, blob, 0.00392, cv::Size(416, 416), cv::Scalar(), true, false, CV_32F);
+    net.setInput(blob);
+    net.forward(detections, output_names);
+
+    std::vector<int> indices[NUM_CLASSES];
+    std::vector<cv::Rect> boxes[NUM_CLASSES];
+    std::vector<float> scores[NUM_CLASSES];
+
+    for (auto& output : detections)
+    {
+        const auto num_boxes = output.rows;
+        for (int i = 0; i < num_boxes; i++)
+        {
+            auto x = output.at<float>(i, 0) * frame.cols;
+            auto y = output.at<float>(i, 1) * frame.rows;
+            auto width = output.at<float>(i, 2) * frame.cols;
+            auto height = output.at<float>(i, 3) * frame.rows;
+            cv::Rect rect(x - width / 2, y - height / 2, width, height);
+
+            for (int c = 0; c < NUM_CLASSES; c++)
+            {
+                auto confidence = *output.ptr<float>(i, 5 + c);
+                if (confidence >= CONFIDENCE_THRESHOLD)
+                {
+                    boxes[c].push_back(rect);
+                    scores[c].push_back(confidence);
+                }
+            }
+        }
+    }
+
+    for (int c = 0; c < NUM_CLASSES; c++)
+        cv::dnn::NMSBoxes(boxes[c], scores[c], 0.0, NMS_THRESHOLD, indices[c]);
+
+    for (int c = 0; c < NUM_CLASSES; c++)
+    {
+        for (size_t i = 0; i < indices[c].size(); ++i)
+        {
+            const auto color = colors[c % NUM_COLORS];
+
+            auto idx = indices[c][i];
+            const auto& rect = boxes[c][idx];
+            cv::rectangle(frame, cv::Point(rect.x, rect.y), cv::Point(rect.x + rect.width, rect.y + rect.height), color, 1);
+
+            std::ostringstream label_ss;
+            label_ss << class_names[c] << ": " << std::fixed << std::setprecision(2) << scores[c][idx];
+            auto label = label_ss.str();
+
+            int baseline;
+            auto label_bg_sz = cv::getTextSize(label.c_str(), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, 1, &baseline);
+            cv::putText(frame, label.c_str(), cv::Point(rect.x, rect.y - baseline - 5), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.7, cv::Scalar(0, 0, 255));
+        }
+
+    }
+
+    cv::imwrite("out.jpg", frame);
+    return 0;
+}
+
